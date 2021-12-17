7016fe4d7a4c576cd907bdf6b842225dfdefb6a7
Minksoleopc <minskoleopc@gmail.com>
2021-12-17 07:51:50 +0530

    api example

---
+++ cypress/integration/api/api4.spec.js
@@ -0,0 +1,40 @@
+// ------ list user of user api 
+// ----->  search the id and valid
+//-----> user that to hit another id
+// ----> response 
+
+describe('verify the GET api', () => {
+
+    let payload = {
+        "name": "morpheus",
+        "job": "leader"
+    }
+
+    it('verify the list user and single user API', () => {
+
+        cy.request({
+            method:"POST",
+            url:"https://reqres.in/api/users",
+            body:payload
+        }).then((response)=>{
+            //cy.log(response.body)
+            expect(response.body).to.have.property('id')
+            return response.body
+
+        })
+        .then(function(res){
+            cy.log(res)
+            cy.request({
+                method:"GET",
+                url:`https://reqres.in/api/users/${res.id}`
+            }).then(function(response){
+                expect(response.body.data.first_name).to.eq(payload['name'])
+            })
+
+        })
+
+    })
+
+})
+
+// 6pm ist
